41e59dc69a4fccfc6a904ff3c653d921c9f6950e
ricky16x <rickx64_64@tutamail.com>
2024-09-13 18:56:29 +0530

    Added placeholder code

---
+++ shell.c
@@ -0,0 +1,14 @@
+#include <stdio.h>																	// Standard library for IO and the libc
+#include <stdlib.h>
+
+#include <unistd.h>																	// This has the syscall unique numbers
+#include <signal.h> 																// Deals with signals
+#include <time.h> 																	// Time functions
+
+
+
+int int main(int argc, char const *argv[])											// placeholder for future commits
+{
+	/* code */
+	return 0;
+}
